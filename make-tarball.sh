75842dbe1ea39cb26989e224c1371c9594b66578
Adeodato Simó <dato@net.com.org.es>
2007-08-12 22:53:29 +0200

    Add script to create release tarballs.

---
+++ make-tarball.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+set -e
+
+PRUNE_FROM_TARBALL="debian make-tarball.sh"
+
+##
+
+if [ ! -e NEWS ]; then
+    echo >&2 "Script must run in the toplevel directory."
+    exit 1
+fi
+
+##
+
+for arg in "$@"; do
+    if [ "$arg" = "-f" ]; then
+    	FORCE=yes
+    fi
+done
+
+##
+
+LINE=$(head -1 NEWS)
+VERSION=$(echo "$LINE" | awk '{print $1}')
+DATE=$(echo "$LINE" | awk '{print $2}')
+
+if ! echo "$DATE" | egrep -q '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'; then
+    if [ -z "$FORCE" ]; then
+    	echo >&2 "Use -f to create a tarball of an unreleased version."
+	exit 1
+    else
+    	APPEND_TO_VER="~`date +%Y-%m-%d`"
+    fi
+fi
+
+##
+
+TMP_DIR=$(env TMPDIR= mktemp -d -p ../tarballs)
+EXPORT_VERSION="minirok-$VERSION$APPEND_TO_VER"
+EXPORT_DIR="$TMP_DIR/$EXPORT_VERSION"
+
+bzr export "$EXPORT_DIR"
+
+( # subshell to preserv old $CWD
+cd "$EXPORT_DIR"
+rm -r $PRUNE_FROM_TARBALL
+
+cd ..
+tar czf "../$EXPORT_VERSION.tar.gz" "$EXPORT_VERSION"
+)
+
+##
+
+rm -r "$EXPORT_DIR"
+rmdir "$TMP_DIR"
