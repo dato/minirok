d9542dbdc19ffb5f93295052eebfd40d9831d49d
Adeodato Simó <dato@net.com.org.es>
2007-08-25 19:11:33 +0200

    Open 0.6.

--- minirok/__init__.py
+++ minirok/__init__.py
@@ -16,7 +16,7 @@ filesystem_encoding = sys.getfilesystemencoding()
 
 __appname__     = 'minirok'
 __progname__    = 'Minirok'
-__version__     = '0.5'
+__version__     = '0.6~dev'
 __description__ = 'A small music player written in Python'
 __copyright__   = 'Copyright (c) 2007 Adeodato Simó'
 __homepage__    = 'http://chistera.yi.org/~adeodato/code/minirok'
