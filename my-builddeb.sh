8998d18f6f84c4e0017b315173a0caaa999d3581
Adeodato Simó <dato@net.com.org.es>
2007-09-11 19:34:53 +0200

    Add a quick hack to build tmp packages.

--- my-builddeb.sh
+++ my-builddeb.sh
@@ -2,12 +2,29 @@
 
 set -e
 
+cd "`dirname $0`"
+
+##
+
+DIST=$(dpkg-parsechangelog | awk '/^Distribution:/ { print $2 }')
+UPSTREAM_VERSION=$(dpkg-parsechangelog |
+    	    	   sed -rne 's/^Version: (.*)-[^-]+$/\1/p')
+
+if [ "$DIST" = "UNRELEASED" ]; then
+    echo >&2 "Building a tmp package, as per changelog"
+    TMP_UPSTREAM_VERSION="$UPSTREAM_VERSION~r`bzr revno`"
+    sed -i -e "1s/ ($UPSTREAM_VERSION-/ ($TMP_UPSTREAM_VERSION-/" debian/changelog
+    fakeroot debian/rules clean binary
+    sed -i -e "1s/ ($TMP_UPSTREAM_VERSION-/ ($UPSTREAM_VERSION-/" debian/changelog
+    rm ../packages/minirok_$UPSTREAM_VERSION~r*deb
+    mv ../minirok_$TMP_UPSTREAM_VERSION-1_all.deb ../packages
+    echo http://chistera.yi.org/~adeodato/code/minirok/packages/minirok_$TMP_UPSTREAM_VERSION-1_all.deb
+    exit 0
+fi
+
 ##
 
 BUILDDIR="../build-area"
-
-UPSTREAM_VERSION=$(dpkg-parsechangelog |
-    	    	   sed -rne 's/^Version: (.*)-[^-]+$/\1/p')
 UPSTREAM_TARBALL="../tarballs/minirok-$UPSTREAM_VERSION.tar.gz"
 DEBIAN_TARBALL="$BUILDDIR/minirok_$UPSTREAM_VERSION.orig.tar.gz"
 
