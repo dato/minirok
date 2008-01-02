36803b93c1b3bc0f64fdd785db1a66f7d50c29ce
Adeodato Simó <dato@net.com.org.es>
2008-01-02 16:43:47 +0100

    Update general copyright to include 2008.

--- minirok/__init__.py
+++ minirok/__init__.py
@@ -18,7 +18,7 @@ __appname__     = 'minirok'
 __progname__    = 'Minirok'
 __version__     = '0.8~dev'
 __description__ = 'A small music player written in Python'
-__copyright__   = 'Copyright (c) 2007 Adeodato Simó'
+__copyright__   = 'Copyright (c) 2007-2008 Adeodato Simó'
 __homepage__    = 'http://chistera.yi.org/~adeodato/code/minirok'
 __bts__         = 'http://bugs.debian.org'
 __authors__     = [
@@ -31,7 +31,7 @@ __thanksto__    = [
 ]
 
 __license__ = '''\
-Minirok is Copyright (c) 2007 Adeodato Simó, and licensed under the
+Minirok is Copyright (c) 2007-2008 Adeodato Simó, and licensed under the
 terms of the MIT license:
 
   Permission is hereby granted, free of charge, to any person obtaining
