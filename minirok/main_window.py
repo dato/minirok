817038320079089ee9661bdf3acc575ff7ae5db4
Adeodato Simó <dato@net.com.org.es>
2008-01-11 16:05:47 +0100

    Add back the shortcut for action_open_directory, which was forgotten while porting.

--- minirok/main_window.py
+++ minirok/main_window.py
@@ -49,7 +49,7 @@ class MainWindow(kdeui.KXmlGuiWindow, util.HasGUIConfig):
 
         # File menu
         self.action_open_directory = util.create_action('action_open_directory',
-                'Open directory...', self.slot_open_directory, 'document-open')
+                'Open directory...', self.slot_open_directory, 'document-open', 'Ctrl+F')
 
         self.action_quit = kdeui.KStandardAction.quit(self.slot_really_quit,
                 actionCollection)
