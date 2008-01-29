2714a0dfdb965fb64d774bded6fdc021a981df36
Adeodato Simó <dato@net.com.org.es>
2008-01-29 19:58:23 +0100

    Overhaul FileListDrag.file_list() (renamed to drag.mimedata_playable_files).
    The behavior is basically the same, with this changes/improvements:
    
      * since we act on a QMimeData object and not an event, we can't use
        isinstance() to know whether to trust the files. So, make FileListDrag
        set a dummy 'text/x-minirok-track-list' mime type in itself, so that
        our QMimeData objects are recognizable.
    
      * do not perform the append_path() dance when the source is trusted;
        saves a stat() per dropped file.
    
      * become resilient against files in the object that do not exist but
        which the engine claims it can play; also refuse to add non-regular
        files.

--- minirok/drag.py
+++ minirok/drag.py
@@ -5,6 +5,7 @@
 # Licensed under the terms of the MIT license.
 
 import os
+import stat
 
 from PyKDE4 import kdecore
 from PyQt4 import QtGui, QtCore
@@ -16,6 +17,8 @@ from minirok import util
 
 class FileListDrag(QtGui.QDrag):
 
+    MIME_TYPE = 'text/x-minirok-track-list'
+
     def __init__(self, files, parent):
         """Constructs a QDrag object from a python list of str paths."""
         nfiles = len(files)
@@ -25,6 +28,9 @@ class FileListDrag(QtGui.QDrag):
             mimedata = QtCore.QMimeData()
             kurllist = kdecore.KUrl.List(map(util.unicode_from_path, files))
             kurllist.populateMimeData(mimedata)
+
+            # flag to signal that the QMimeData comes from ourselves
+            mimedata.setData(self.MIME_TYPE, 'True')
 
             # display a "tooltip" with the number of tracks
             text = '%d track%s' % (nfiles, nfiles > 1 and 's' or '')
@@ -41,32 +47,43 @@ class FileListDrag(QtGui.QDrag):
             self.setMimeData(mimedata)
             self.setPixmap(self.pixmap)
 
-    @staticmethod
-    def file_list(event):
-        """Return a list of the file paths encoded in this event.
+##
 
-        Files that the engine can't play will not be included; if the event is
-        an instance of FileListDrag, though, the list will be assumed to be
-        filtered already.
+def mimedata_playable_files(mimedata):
+    """Return a list of playable stuff from a QMimeData object.
 
-        Directories will be read and all its files included.
-        """
+    The QMimeData object must haveUrls(). Files that the engine can't play
+    will not be included, and directories will be read and all its files
+    included.
+
+    However, if the QMimeData comes from Minirok itself (as determined by
+    the presence of FileListDrag.MIME_TYPE, all URLs will be considered to
+    be files (and not directories) that the engine can play.
+    """
+    assert mimedata.hasUrls(), 'You tried to drop something without URLs.'
+    urls = kdecore.KUrl.List.fromMimeData(mimedata)
+
+    if mimedata.hasFormat(FileListDrag.MIME_TYPE):
+        return map(util.kurl_to_path, urls)
+    else:
         files = []
-        all_playable = isinstance(event, FileListDrag)
-
-        urls = kdecore.KURL.List()
-        kdecore.KURLDrag.decode(event, urls)
-
         def append_path(path):
-            if os.path.isdir(path):
+            try:
+                mode = os.stat(path).st_mode
+            except OSError, e:
+                minirok.logger.warn('ignoring dropped %r: %s' % (path, e))
+                return
+
+            if stat.S_ISDIR(mode):
                 try:
                     contents = sorted(os.listdir(path))
                 except OSError, e:
-                    minirok.logger.warn('could not list directory: %s', e)
+                    minirok.logger.warn('could not list directory %r: %s', e)
                 else:
-                    for file_ in contents:
-                        append_path(os.path.join(path, file_))
-            elif ((all_playable or minirok.Globals.engine.can_play(path))
+                    for entry in contents:
+                        append_path(os.path.join(path, entry))
+            elif (stat.S_ISREG(mode)
+                    and minirok.Globals.engine.can_play(path)
                     and path not in files):
                 files.append(path)
 
