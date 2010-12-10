a135ac87c90c47958659015cfeac2efdddb1c122
Adeodato Simó <dato@net.com.org.es>
2010-12-10 18:20:32 +0000

    Add BaseTest.TerminateEarly exception class.

--- minirok/tests/__init__.py
+++ minirok/tests/__init__.py
@@ -17,8 +17,21 @@ class BaseTest(unittest2.TestCase):
     """Base class for all Minirok tests."""
 
     def setUp(self):
+        class TerminateEarly:
+            """Exception for terminating tested code early.
+
+            This exception only exists in running tests, and hence cannot be
+            caught by tested code by name, ensuring it'll always terminate
+            running code unless they do a blanket "except:" statement.
+
+            It doesn't inherit from Exception to go over "except Exception", but
+            can't inherit from object either, since new-style classes can only
+            be raised if they inherit from BaseException.
+            """
+
         self.mox = mox.Mox()
         self.stubs = mox.stubout.StubOutForTesting()
+        self.TerminateEarly = TerminateEarly
 
     def tearDown(self):
         self.mox.UnsetStubs()
